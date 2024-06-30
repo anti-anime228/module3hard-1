bc293e12cc057739025cb7497cc0b93e32fef744
anti-anime228 <qwerty9228@bk.ru>
2024-06-30 12:01:24 +0300

    Add files via upload

---
+++ module3hard.py
@@ -0,0 +1,24 @@
+data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
+                  ((), [{(2, 'Urban', ('Urban2', 35))}])]
+
+
+def calculate_structure_sum(data_structure):
+    count_number = 0
+    count_string = 0
+
+    if isinstance(data_structure, int):
+        count_number += data_structure
+    elif isinstance(data_structure, str):
+        count_string += len(data_structure)
+    elif isinstance(data_structure, (list, tuple, set)):
+        for item in data_structure:
+            count_number += calculate_structure_sum(item)
+    elif isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            count_number += calculate_structure_sum(key)
+            count_string += calculate_structure_sum(value)
+    return count_number + count_string
+
+
+result = calculate_structure_sum(data_structure)
+print(result)
